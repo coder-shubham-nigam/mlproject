72247e376acf1fbd68b922a2f08ff7fa040536d8
coder-shubham-nigam <shubham.nigam2005@gmail.com>
2026-09-06 15:32:04 +0530

    Model Trainer done

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,8 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs")
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+logs_path = os.path.join(project_root, "logs")
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path , LOG_FILE)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 import dill # type: ignore
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path , obj):
     try:
@@ -14,5 +15,25 @@ def save_object(file_path , obj):
         with open(file_path , 'wb') as f:
             dill.dump(obj,f)
     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(xtrain , ytrain , xtest , ytest, models):
+    try:        
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(xtrain, ytrain) # Train model
+
+            # Make predictions
+            y_train_pred = model.predict(xtrain)
+            y_test_pred = model.predict(xtest)
+            
+            train_model_score = r2_score(ytrain , y_train_pred)
+            test_model_score = r2_score(ytest , y_test_pred)
+            
+            report[model_name] = test_model_score
+            
+        return report
     except Exception as e:
         raise CustomException(e,sys)

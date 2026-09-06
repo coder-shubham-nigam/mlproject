e5aa9492e58f5610bc78c7e6c8e137929b95fd68
coder-shubham-nigam <shubham.nigam2005@gmail.com>
2026-09-06 16:20:39 +0530

    Hyper Parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,17 +37,51 @@ class ModelTrainer:
             )
             
             models = {
-                "random Forest" : RandomForestRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "K-Neighbors Regression" : KNeighborsRegressor(),
-                "XBG Regressor" : XGBRegressor() ,
-                "CatBoost Regressor" : CatBoostRegressor(verbose = False),
-                "AdaBoost Regressor" : AdaBoostRegressor()
+            "Random Forest": RandomForestRegressor(),
+            "Decision Tree": DecisionTreeRegressor(),
+            "Gradient Boosting": GradientBoostingRegressor(),
+            "Linear Regression": LinearRegression(),
+            "K-Neighbors Regression": KNeighborsRegressor(),
+            "XGBRegressor": XGBRegressor(),
+            "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+            "AdaBoost Regressor": AdaBoostRegressor(),
             }
+
+            params = {
+                "Random Forest": {
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                },
+                "Gradient Boosting": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
+
             
-            model_report:dict = evaluate_models(xtrain = xtrain , ytrain = ytrain , xtest = xtest , ytest = ytest, models = models)
+            model_report:dict = evaluate_models(xtrain = xtrain , ytrain = ytrain , xtest = xtest , ytest = ytest, models = models , params = params)
             
             best_model_score = max(sorted(model_report.values()))
             
@@ -60,6 +94,7 @@ class ModelTrainer:
             if best_model_score < 0.6:
                 raise CustomException("No best model found", sys)
             logging.info("best model found on both training and testing dataset")
+            logging.info(f"Best model is {best_model_name}")
             
             save_object(
                 file_path= self.model_trainer_config.trained_model_file_path,
